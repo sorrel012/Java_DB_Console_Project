687bfe6810d8ff0270d00db51b3423d6d37d5b93
sorrel012 <wain1719@gmail.com>
2023-04-24 23:59:37 +0900

    feat: create file to connect jdbc

---
+++ stock-project/src/com/db/DBUtil.java
@@ -0,0 +1,31 @@
+package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+
+    public static Connection open() {
+        String url = "jdbc:oracle:thin:@localhost:1521:xe";
+        String id = "st";
+        String pw = "1234";
+
+        try {
+
+            Connection conn = null;
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            conn =   DriverManager.getConnection(url,id,pw);
+
+            return conn;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+}
