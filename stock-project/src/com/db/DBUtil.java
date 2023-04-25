97d899a0c535ee1401fe2031e6ae99eaa1a5d97d
hwh <sorrel012@gmail.com>
2023-04-26 08:54:14 +0900

    style: remove whitespaces

--- stock-project/src/com/db/DBUtil.java
+++ stock-project/src/com/db/DBUtil.java
@@ -16,7 +16,7 @@ public class DBUtil {
 
             Class.forName("oracle.jdbc.driver.OracleDriver");
 
-            conn =   DriverManager.getConnection(url,id,pw);
+            conn = DriverManager.getConnection(url,id,pw);
 
             return conn;
             
