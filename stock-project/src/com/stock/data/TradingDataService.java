e49ca9166a1eea6b1371561c523e38bff7f0296e
hwh <sorrel012@gmail.com>
2023-04-26 09:16:18 +0900

    fix: revise variable

--- stock-project/src/com/stock/data/TradingDataService.java
+++ stock-project/src/com/stock/data/TradingDataService.java
@@ -1,14 +1,12 @@
 package com.stock.data;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
-import com.db.DBUtil;
 /**
  * 국내 증시 사이트에서 시세 정보를 읽어오는 클래스
  * @author 6조
@@ -19,7 +17,7 @@ public class TradingDataService {
     private static final String KOSPI = "https://finance.naver.com/sise/sise_market_sum.naver?sosok=0&page="; // 코스피
     private static final String KOSDAQ = "https://finance.naver.com/sise/sise_market_sum.naver?sosok=1&page="; // 코스닥
     public static final String KOSPI_LIST = ".\\dat\\stockprice\\kospiList.txt";
-    public static final String KOSPI_LIST = ".\\dat\\stockprice\\kosdaqList.txt";
+    public static final String KOSDAQ_LIST = ".\\dat\\stockprice\\kosdaqList.txt";
 
     /**
      * 코스피, 코스닥의 변동률을 읽어오는 매소드
@@ -27,7 +25,7 @@ public class TradingDataService {
      */
     public static void createStockData() {
         
-        readStockData(KOSPI_LIST, KOSPI);
+        readStockData(KOSPI_LIST, KOSPI,41);
     }
 
     /**
@@ -35,7 +33,7 @@ public class TradingDataService {
      * @param stockUrl 증권 사이트 주소
      * @param pageNum 읽어올 페이지 수
      */
-    private static void readStockData(String fileUrl, String stockUrl) {
+    private static void readStockData(String fileUrl, String stockUrl, int pageNum) {
         
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileUrl));
@@ -43,7 +41,7 @@ public class TradingDataService {
             
             for(int i = 1; i <= pageNum; i++) {
                 
-                Document doc = Jsoup.connect(stockUrl+pageNum).get(); // 웹에서 내용을 가져온다
+                Document doc = Jsoup.connect(stockUrl+i).get(); // 웹에서 내용을 가져온다
 
                 // 페이지 전체 읽어오기
                 Elements stockData = doc.select("td");
@@ -105,10 +103,9 @@ public class TradingDataService {
                         }
 
                     }
+                    line += String.format("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", seq, stockName,nowPrice,rate,volume,per,roe);
                     
                 }
-
-                line += String.format("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", seq, stockName,nowPrice,rate,volume,per,roe);
                 
             }
             
