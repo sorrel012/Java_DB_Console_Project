77734c53473a52b4249cf54cc4d05decb14559c3
sorrel012 <wain1719@gmail.com>
2023-04-27 23:33:07 +0900

    refactor: Replace the txt file with database data

--- stock-project/src/com/stock/data/TradeData.java
+++ stock-project/src/com/stock/data/TradeData.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import com.stock.service.signup.Login;
 
@@ -16,281 +17,254 @@ import com.stock.service.signup.Login;
  *
  */
 public class TradeData {
-	
-	public final static String HISTORYDIR = ".\\dat\\tradinghistory\\";
-	public final static String FILEPATH = HISTORYDIR + Login.loginUser.getId() + "\\" + "buyList.txt";
-	public final static String HISTORYFILE = HISTORYDIR + Login.loginUser.getId() + "\\" + "history.txt";
 
-	private String stockName;				//주식명
-	private String volume;					//매수량
-	private String priceNow;				//현재가
-	private int totalPrice;					//총 매수금액 / 매도금액
-	private int totalAssets;				//총 자산
-	private int totalAvailableAssets;		//총 가용자산
-	
-	private ArrayList<String> list = new ArrayList<String>(5);
-	
-	public TradeData(String stockName, String volume) {
-		this.stockName = stockName;
-		this.volume = volume;
-	}
+    public final static String HISTORYDIR = ".\\dat\\tradinghistory\\";
+    public final static String FILEPATH = HISTORYDIR + Login.loginUser.getId() + "\\" + "buyList.txt";
+    public final static String HISTORYFILE = HISTORYDIR + Login.loginUser.getId() + "\\" + "history.txt";
 
-	public String getStockName() {
-		return stockName;
-	}
+    private String stockName;				//주식명
+    private String volume;					//매수량
+    private String priceNow;				//현재가
+    private int totalPrice;					//총 매수금액 / 매도금액
+    private int totalAssets;				//총 자산
+    private int totalAvailableAssets;		//총 가용자산
 
-	public void setStockName(String stockName) {
-		this.stockName = stockName;
-	}
+    private List<List<String>> allStocks = AllStockList.storeAllStockList();
 
-	public String getVolume() {
-		return volume;
-	}
+    public TradeData(String stockName, String volume) {
+        this.stockName = stockName;
+        this.volume = volume;
+    }
 
-	public void setVolume(String volume) {
-		this.volume = volume;
-	}
+    public String getStockName() {
+        return stockName;
+    }
 
-	public String getPriceNow() {
-		return priceNow;
-	}
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
 
-	public void setPriceNow(String priceNow) {
-		this.priceNow = priceNow;
-	}
+    public String getVolume() {
+        return volume;
+    }
 
-	public int getTotalPrice() {
-		return Integer.parseInt(priceNow.replaceAll(",", "")) * Integer.parseInt(volume.replaceAll(",", ""));
-	}
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
 
-	public void setTotalPrice(int totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-	
-	/**
-	 * 보유한 모든 주식의 총 가격 정보를 만드는 메소드
-	 */
-	public int getTotalAssets() {
+    public String getPriceNow() {
+        return priceNow;
+    }
 
-		int sum = 0;
-		
-		for (int i = 0; i < list.size(); i++) {
-			
-			try {
-				
-				BufferedReader listReader = new BufferedReader(new FileReader(getList().get(i)));
-				BufferedReader buyReader = new BufferedReader(new FileReader(TradeData.FILEPATH));
-				
-				ArrayList<String> buyName = new ArrayList<String>();
-				ArrayList<String> volume = new ArrayList<String>();
-				
-				String line = null;
-				
-				while ((line = buyReader.readLine()) != null) {
-					
-					String[] temp = line.split("■");
-					
-					buyName.add(temp[0]);
-					volume.add(temp[1]);
-					
-				}
-				
-				line = null;
-				
-				while ((line = listReader.readLine()) != null) {
-					
-					String[] temp = line.split("■");
-					
-					for (int j = 0; j < buyName.size(); j++) {
-						
-						int stockPrice = 0;
-						
-						if (temp[1].equals(buyName.get(j))) {
-							stockPrice = Integer.parseInt(temp[2].replaceAll(",", ""));
-							sum += stockPrice * Integer.parseInt(volume.get(j));
-							break;
-						}
-						
-					}
-					
-				}
-				
-				listReader.close();
-				buyReader.close();
-				
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-		} //for
+    public void setPriceNow(String priceNow) {
+        this.priceNow = priceNow;
+    }
 
-		return sum;
+    public int getTotalPrice() {
+        return Integer.parseInt(priceNow.replaceAll(",", "")) * Integer.parseInt(volume.replaceAll(",", ""));
+    }
 
-	}
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 
-	public void setTotalAssets(int totalAssets) {
-		this.totalAssets = totalAssets;
-	}
+    /**
+     * 보유한 모든 주식의 총 가격 정보를 만드는 메소드
+     */
+    public int getTotalAssets() {
 
-	public int getTotalAvailableAssets() {
-		return Login.loginUser.getAvailableAssets() - getTotalPrice();
-	}
+        int sum = 0;
 
-	public void setTotalAvailableAssets(int totalAvailableAssets) {
-		this.totalAvailableAssets = totalAvailableAssets;
-	}
+        try {
 
-	public ArrayList<String> getList() {
-		return list;
-	}
+            allStocks = AllStockList.storeAllStockList();
 
-	public void setList(ArrayList<String> list) {
-		this.list = list;
-	}
+            BufferedReader buyReader = new BufferedReader(new FileReader(TradeData.FILEPATH));
 
-	public void readAll() {
+            ArrayList<String> buyName = new ArrayList<String>();
+            ArrayList<String> volume = new ArrayList<String>();
 
-		list.add(TradingDataService.KOSPI_RISE_LIST);
-		list.add(TradingDataService.KOSPI_FALL_LIST);
-		list.add(TradingDataService.KOSPI_STEADY_LIST);
-		list.add(TradingDataService.KOSDAQ_RISE_LIST);
-		list.add(TradingDataService.KOSDAQ_FALL_LIST);
-		list.add(TradingDataService.KOSDAQ_STEADY_LIST);
+            String line = null;
 
-	}
-	
-	/**
-	 * 매수 & 매도 히스토리를 텍스트 파일로 생성하는 메소드
-	 */
-	public static void createHistory(TradeData td, String type) {
-		
-		try {
-			
-			Calendar cal = Calendar.getInstance();
-			String date = String.format("%tF", cal);
-			
-			File dir = new File(HISTORYDIR + Login.loginUser.getId());
-			dir.mkdir();
-			
-			File file = new File(HISTORYDIR + Login.loginUser.getId() + "\\" + "history.txt");
-			file.createNewFile();
-			
-			BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
-			
-			if (file.exists()) {
-				
-				String content = String.format("%s■%s■%s■%,d■%s■%s■%s\n", Login.loginUser.getId(), td.getStockName(), td.getPriceNow(), td.getTotalPrice(), date, type, td.getVolume());
-				bw.write(content);
-				System.out.printf("\t\t\t\t\t\t\t\t%s가 완료되었습니다.\n", type);
-				bw.close();
-			}
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	/**
-	 * 매수한 주식의 정보를 텍스트 파일로 생성하는 메소드
-	 */
-	public static void createBuyList(TradeData td, String type) {
-		
-		try {
-			
-			File file = new File(TradeData.FILEPATH);
-			
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-			
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			
-			String line = null;
-			
-			String content = "";
-			
-			while ((line = br.readLine()) != null) {
-				
-				String[] temp = line.split("■");
-				
-				if (temp[0].equals(td.getStockName())) {
-					int volume = Integer.parseInt(temp[1]) + Integer.parseInt(td.getVolume());
-					content += td.getStockName() + "■" + volume + "\n";
-					
-				} else {
-					content += line + "\n";
-				}
-				
-			}
-			br.close();
-			
-			if (!content.contains(td.getStockName())) {
-				content += String.format("%s■%s\n", td.getStockName(), td.getVolume());
-			}
-			
-			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-			bw.write(content);
-			bw.close();
-			
-			Member.updateAccountInfo(td, type);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	/**
-	 * 매도한 주식정보를 매수 정보가 들어있는 텍스트파일에서 지우는 메소드
-	 */
-	public static void removeBuyList(TradeData td, String type) {
+            while ((line = buyReader.readLine()) != null) {
 
-		try {
+                String[] temp = line.split("■");
 
-			File file = new File(TradeData.FILEPATH);
+                buyName.add(temp[0]);
+                volume.add(temp[1]);
 
-			if (!file.exists()) {
-				file.createNewFile();
-			}
+            }
 
-			BufferedReader br = new BufferedReader(new FileReader(file));
+            for (List<String> tmpList : allStocks) {
 
-			String line = null;
+                for (int j = 0; j < buyName.size(); j++) {
 
-			String content = "";
+                    int stockPrice = 0;
 
-			while ((line = br.readLine()) != null) {
+                    if (tmpList.get(1).equals(buyName.get(j))) {
+                        stockPrice = Integer.parseInt(tmpList.get(2).replaceAll(",", ""));
+                        sum += stockPrice * Integer.parseInt(volume.get(j));
+                        break;
+                    }
 
-				String[] temp = line.split("■");
+                }
 
-				if (temp[0].equals(td.getStockName())) {
-					int volume = Integer.parseInt(temp[1]) - Integer.parseInt(td.getVolume());
-					content += td.getStockName() + "■" + volume + "\n";
+            }
 
-				} else {
-					content += line + "\n";
-				}
+            buyReader.close();
 
-			}
-			br.close();
 
-			if (!content.contains(td.getStockName())) {
-				content += String.format("%s■%s\n", td.getStockName(), td.getVolume());
-			}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-			bw.write(content);
-			bw.close();
+        return sum;
 
-			Member.updateAccountInfo(td, type);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setTotalAssets(int totalAssets) {
+        this.totalAssets = totalAssets;
+    }
+
+    public int getTotalAvailableAssets() {
+        return Login.loginUser.getAvailableAssets() - getTotalPrice();
+    }
+
+    public void setTotalAvailableAssets(int totalAvailableAssets) {
+        this.totalAvailableAssets = totalAvailableAssets;
+    }
+
+    /**
+     * 매수 & 매도 히스토리를 텍스트 파일로 생성하는 메소드
+     */
+    public static void createHistory(TradeData td, String type) {
+
+        try {
+
+            Calendar cal = Calendar.getInstance();
+            String date = String.format("%tF", cal);
+
+            File dir = new File(HISTORYDIR + Login.loginUser.getId());
+            dir.mkdir();
+
+            File file = new File(HISTORYDIR + Login.loginUser.getId() + "\\" + "history.txt");
+            file.createNewFile();
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+
+            if (file.exists()) {
+
+                String content = String.format("%s■%s■%s■%,d■%s■%s■%s\n", Login.loginUser.getId(), td.getStockName(), td.getPriceNow(), td.getTotalPrice(), date, type, td.getVolume());
+                bw.write(content);
+                System.out.printf("\t\t\t\t\t\t\t\t%s가 완료되었습니다.\n", type);
+                bw.close();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 매수한 주식의 정보를 텍스트 파일로 생성하는 메소드
+     */
+    public static void createBuyList(TradeData td, String type) {
+
+        try {
+
+            File file = new File(TradeData.FILEPATH);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line = null;
+
+            String content = "";
+
+            while ((line = br.readLine()) != null) {
+
+                String[] temp = line.split("■");
+
+                if (temp[0].equals(td.getStockName())) {
+                    int volume = Integer.parseInt(temp[1]) + Integer.parseInt(td.getVolume());
+                    content += td.getStockName() + "■" + volume + "\n";
+
+                } else {
+                    content += line + "\n";
+                }
+
+            }
+            br.close();
+
+            if (!content.contains(td.getStockName())) {
+                content += String.format("%s■%s\n", td.getStockName(), td.getVolume());
+            }
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.write(content);
+            bw.close();
+
+            Member.updateAccountInfo(td, type);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 매도한 주식정보를 매수 정보가 들어있는 텍스트파일에서 지우는 메소드
+     */
+    public static void removeBuyList(TradeData td, String type) {
+
+        try {
+
+            File file = new File(TradeData.FILEPATH);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line = null;
+
+            String content = "";
+
+            while ((line = br.readLine()) != null) {
+
+                String[] temp = line.split("■");
+
+                if (temp[0].equals(td.getStockName())) {
+                    int volume = Integer.parseInt(temp[1]) - Integer.parseInt(td.getVolume());
+                    content += td.getStockName() + "■" + volume + "\n";
+
+                } else {
+                    content += line + "\n";
+                }
+
+            }
+            br.close();
+
+            if (!content.contains(td.getStockName())) {
+                content += String.format("%s■%s\n", td.getStockName(), td.getVolume());
+            }
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.write(content);
+            bw.close();
+
+            Member.updateAccountInfo(td, type);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
