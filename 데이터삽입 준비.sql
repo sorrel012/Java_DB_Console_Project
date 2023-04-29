432803e050a865d15ec463d1109f9020d3a88b3e
sorrel012 <wain1719@gmail.com>
2023-04-29 21:22:56 +0900

    chore: practice to create view

--- 데이터삽입 준비.sql
+++ 데이터삽입 준비.sql
@@ -6,3 +6,34 @@ select count(*) from tblkosdaq;
 
 ALTER system SET processes=100000 scope=spfile;
 COMMIT;
+
+create or replace view vwKospiRise
+as
+select * from tblkospi
+    where (SUBSTR(rate, 1, 1) = '+')
+        order by SUBSTR(rate, 2) desc;
+        
+        
+create or replace view vwKospiFall
+as
+select * from tblkospi
+    where (SUBSTR(rate, 1, 1) = '-')
+        order by SUBSTR(rate, 2) desc;
+
+create or replace view vwKosdaqRise
+as
+select * from tblkosdaq
+    where (SUBSTR(rate, 1, 1) = '+')
+        order by SUBSTR(rate, 2) desc;
+        
+        
+create or replace view vwKosdaqFall
+as
+select * from tblkosdaq
+    where (SUBSTR(rate, 1, 1) = '-')
+        order by SUBSTR(rate, 2) desc;        
+        
+        
+        
+        
+        
