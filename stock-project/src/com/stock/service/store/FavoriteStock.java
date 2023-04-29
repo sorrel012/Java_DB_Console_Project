22a9d5016ac2d3d498a1ee985cb29f0842c6dc36
sorrel012 <wain1719@gmail.com>
2023-04-30 01:58:17 +0900

    refactor: replace stocklist txt file with DB tblStock

--- stock-project/src/com/stock/service/store/FavoriteStock.java
+++ stock-project/src/com/stock/service/store/FavoriteStock.java
@@ -1,8 +1,5 @@
 package com.stock.service.store;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,117 +14,85 @@ import com.stock.view.MyStock;
  */
 public class FavoriteStock {
 
-	/**
-	 * 관심 종목 UI 출력 리스트
-	 */
-	public static void favorite() {
-		
-			System.out.println();
-			System.out.println("\t\t\t\t\t\t===================================================================================================");
-			System.out.println("\t\t\t\t\t\t\t\t\t\t\t　　　[My Stock - 관심 종목]");
-			System.out.println("\t\t\t\t\t\t===================================================================================================");
-			System.out.println();
-			System.out.println("\t\t\t\t\t\t　[종목명]\t\t\t[현재가]\t\t[등락률]\t\t[거래량]\t\t[ PER ]\t\t[ ROE ]");
-			
-			StockList();
+    /**
+     * 관심 종목 UI 출력 리스트
+     */
+    public static void favorite() {
 
-			
-			System.out.println();
-			System.out.println("\t\t\t\t\t\t===================================================================================================");
-			System.out.println();
-			
-			inputEnterMyStock();
-			
-					
-	}
-	
-	/**
-	 * "계속하시려면 엔터를 입력해주세요."를 출력하는 메소드
-	 * 엔터 입력 시 My Stock UI로 돌아감
-	 */
-	private static void inputEnterMyStock() {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println();
-		System.out.print("\t\t\t\t\t\t\t\t\t\t 　　　계속하시려면 엔터를 입력해주세요.");
-		String enterLine = scan.nextLine();
-		MyStock.UI();
-		
-		scan.close();
-	}
-	
-	
-	/**
-	 * 관심 종목으로 등록되어있는 종목 이름
-	 */
-	private static void StockList() {
-		
-		try {
-			BufferedReader stockListReader = new BufferedReader(new FileReader(".\\dat\\storage\\stockList.txt"));
-			
-			//List<String> stocklist = new ArrayList<String>();
-			String line = null;
+        System.out.println();
+        System.out.println("\t\t\t\t\t\t===================================================================================================");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t　　　[My Stock - 관심 종목]");
+        System.out.println("\t\t\t\t\t\t===================================================================================================");
+        System.out.println();
+        System.out.println("\t\t\t\t\t\t　[종목명]\t\t\t[현재가]\t\t[등락률]\t\t[거래량]\t\t[ PER ]\t\t[ ROE ]");
 
-			while((line = stockListReader.readLine())!= null) {
-				
-				String[] temp = line.split(",");
-				if (Login.loginUser.getNo().equals(temp[0])) {
-					MyStockList();
-				}
-			}
-			
-			stockListReader.close();
-	
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	/**
-	 * 관심 종목으로 등록한 종목의 내용 출력
-	 * (종목명, 현재가, 등락률, 거래량, 매수호가, 매도호가)
-	 */
-	private static void MyStockList() {
-		
-		List<List<String>> list = AllStockList.storeAllStockList();
-		
-		try {
-			//TODO 관심 주식 목록 수정
-			BufferedReader stockListReader = new BufferedReader(new FileReader(".\\dat\\storage\\stockList.txt"));
-			String line = null;
-			
-				while((line = stockListReader.readLine())!= null) {
-				
-				String[] temp = line.split(",");
-					
-				for(int i=1; i<temp.length;i++) {
-					for(int j=0; j<list.size(); j++) {
-						if (temp[i].equals(list.get(j).get(1))) {
-							System.out.printf("\t\t\t\t\t\t　%-10s\t\t%6s\t\t%5s\t%15s\t\t%7s\t\t%7s\n"
-													, list.get(j).get(1)
-													, list.get(j).get(2)
-													, list.get(j).get(4)
-													, list.get(j).get(5)
-													, list.get(j).get(6)
-													, list.get(j).get(7));
-							break;
-						}
-					}
-					
-				}
-				
-			}
-				
-			stockListReader.close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
+        printMyStockList();
+
+        System.out.println();
+        System.out.println("\t\t\t\t\t\t===================================================================================================");
+        System.out.println();
+
+        inputEnterMyStock();
+
+    }
+
+
+    /**
+     * "계속하시려면 엔터를 입력해주세요."를 출력하는 메소드
+     * 엔터 입력 시 My Stock UI로 돌아감
+     */
+    private static void inputEnterMyStock() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println();
+        System.out.print("\t\t\t\t\t\t\t\t\t\t 　　　계속하시려면 엔터를 입력해주세요.");
+        String enterLine = scan.nextLine();
+        MyStock.UI();
+
+        scan.close();
+    }
+
+
+    /**
+     * 로그인 한 회원의 관심 주식 목록을 출력하는 메소드 오는 메소드
+     */
+    private static void printMyStockList() {
+
+        List<List<String>> userStorageList = UserStockStorage.load();
+
+        for(List<String> tmpUserStorage : userStorageList) {
+
+            if (Login.loginUser.getNo().equals(tmpUserStorage.get(0))) {
+                myStockList(tmpUserStorage.get(1));
+            }
+        }
+
+    }
+
+
+    /**
+     * 관심 종목으로 등록한 종목의 정보를 탐색하는 메소드
+     * (종목명, 현재가, 등락률, 거래량, PER, ROE)
+     */
+    private static void myStockList(String stockName) {
+
+        List<List<String>> list = AllStockList.storeAllStockList();
+
+        for(List<String> tmpList : list) {
+            if(tmpList.get(1).equals(stockName)) {
+                System.out.printf("\t\t\t\t\t\t　%-10s\t\t%6s\t\t%5s\t%15s\t\t%7s\t\t%7s\n"
+                        , tmpList.get(1)
+                        , tmpList.get(2)
+                        , tmpList.get(3)
+                        , tmpList.get(4)
+                        , tmpList.get(5)
+                        , tmpList.get(6));
+                break;
+            }
+        }
+    }
+
 }
 
 
