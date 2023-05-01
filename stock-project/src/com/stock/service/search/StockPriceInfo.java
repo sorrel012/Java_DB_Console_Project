7dd068c754625cbaba1424486a04e937b5db8c20
hwh <sorrel012@gmail.com>
2023-05-01 09:38:07 +0900

    fix: change data type of creating view queries

--- stock-project/src/com/stock/service/search/StockPriceInfo.java
+++ stock-project/src/com/stock/service/search/StockPriceInfo.java
@@ -71,19 +71,19 @@ public class StockPriceInfo {
             st = con.createStatement();
             
             String sql = "create or replace view vwKospiRise as "
-                    + "select * from tblkospi where (SUBSTR(rate, 1, 1) = '+') order by SUBSTR(rate, 2) desc";
+                    + "select * from tblkospi where (SUBSTR(rate, 1, 1) = '+') order by TO_NUMBER(SUBSTR(rate, 2)) desc";
             st.executeUpdate(sql);
             
             sql = "create or replace view vwKospiFall as "
-                    + "select * from tblkospi where (SUBSTR(rate, 1, 1) = '-') order by SUBSTR(rate, 2) desc";
+                    + "select * from tblkospi where (SUBSTR(rate, 1, 1) = '-') order by TO_NUMBER(SUBSTR(rate, 2)) desc";
             st.executeUpdate(sql);
             
             sql = "create or replace view vwKosdaqRise as "
-                    + "select * from tblkosdaq where (SUBSTR(rate, 1, 1) = '+') order by SUBSTR(rate, 2) desc";
+                    + "select * from tblkosdaq where (SUBSTR(rate, 1, 1) = '+') order by TO_NUMBER(SUBSTR(rate, 2)) desc";
             st.executeUpdate(sql);
             
             sql = "create or replace view vwKosdaqFall as "
-                    + "select * from tblkosdaq where (SUBSTR(rate, 1, 1) = '-') order by SUBSTR(rate, 2) desc";
+                    + "select * from tblkosdaq where (SUBSTR(rate, 1, 1) = '-') order by TO_NUMBER(SUBSTR(rate, 2)) desc";
             st.executeUpdate(sql);
             
             st.close();
